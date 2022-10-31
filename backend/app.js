882f6e0abc043f63db75a6ff6c5c2b15d217ede2
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-11-01 03:21:31 +0530

    Crreated Models for expense entrie

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,33 @@
-const dotenv = require('dotenv')
-
+const dotenv=require('dotenv')
 dotenv.config()
-
 const express=require('express')
 
 const bodyParser=require('body-parser')
 
 const sequelize=require('./util/database')
 
+const User=require('./models/user')
+const Expense=require('./models/expense')
+
 const cors=require('cors')
 
 const authRoutes=require('./routes/auth')
+const expenseRoutes=require('./routes/expense')
 
 const app=express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cors());
 
-app.use('/user',authRoutes);
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
-sequelize
-// .sync({force:true})
-.sync()
+app.use('/user',authRoutes);
+app.use(expenseRoutes)
+
+sequelize.sync()
 .then(()=>{
     app.listen(5000)
 })
