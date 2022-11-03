14dfc18b6ef8f3b66c14d25902ba118b6775dd7f
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-11-03 13:03:36 +0530

    Forget password feature intigrated successfully

--- backend/app.js
+++ backend/app.js
@@ -6,17 +6,20 @@ dotenv.config();
 const express = require('express');
 const bodyParser=require('body-parser')
 
-const sequelize=require('./util/database')
+const sequelize = require('./util/database');
+
 const User = require('./models/user');
 const Expense = require('./models/expense');
 const Order = require('./models/order');
+const Password=require('./models/password')
 
 
 const cors=require('cors')
 
 const authRoutes=require('./routes/auth')
 const expenseRoutes=require('./routes/expense')
-const premiumRoutes=require('./routes/premium')
+const premiumRoutes = require('./routes/premium')
+const forgotPasswordRoutes=require('./routes/forgotPassword')
 
 
 const app=express();
@@ -30,10 +33,13 @@ User.hasMany(Expense);
 Expense.belongsTo(User);
 User.hasOne(Order)
 Order.belongsTo(User)
+User.hasMany(Password)
+Password.belongsTo(User)
 
 app.use('/user',authRoutes);
 app.use(expenseRoutes)
 app.use(premiumRoutes)
+app.use(forgotPasswordRoutes);
 
 sequelize.sync()
 .then(()=>{
